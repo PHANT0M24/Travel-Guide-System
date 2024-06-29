fd8ddeedb3471f6c4f93063f422669d2ab99b241
najmul193 <najmulalamrivan@gmail.com>
2024-06-29 22:36:44 +0600

    Initial

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -21,61 +21,6 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="flex w-full flex-col">
-
-        <div className="divider"></div>
-        <div className="card bg-base-300 rounded-box grid h-20 place-items-center">content</div>
-        <div className="divider"></div>
-
-      </div>
-      <div className="carousel rounded-box">
-        <div className="carousel-item">
-          <div className="card card-compact bg-base-100 w-96 shadow-xl">
-            <figure>
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Buy Now</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
-            alt="Burger" />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
-            alt="Burger" />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
-            alt="Burger" />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
-            alt="Burger" />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
-            alt="Burger" />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
-            alt="Burger" />
-        </div>
-      </div>
     </div>
   );
 };
