8f6b7cd2bf77c38560f2bfce56d9d15f91b9b521
khairulanam23 <mka.rifat.24@gmail.com>
2024-06-29 08:09:31 +0600

    first commit

---
+++ client/travelGuideSystem/src/main.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./Components/Errorpage/Errorpage.jsx";
+import Home from "./Components/Home/Home.jsx";
+import Register from "./Components/Register/Register.jsx";
+import Login from "./Components/Login/Login.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/register",
+        element: <Register/>
+      },
+      {
+        path: "/login",
+        element: <Login/>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
